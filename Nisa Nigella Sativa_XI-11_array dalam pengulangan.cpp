3ad1d51c31d33803cb82250ec18ca461f8a77e47
nigella11 <141220089+nigella11@users.noreply.github.com>
2023-08-02 15:14:58 +0700

    Add files via upload

---
+++ Nisa Nigella Sativa_XI-11_array dalam pengulangan.cpp
@@ -0,0 +1,26 @@
+  #include <cstdlib>
+  #include <iostream>
+  using namespace std;
+  int main()
+  {
+  	int 1;
+  	float bil[7] , jumlah=0.0. rata2;
+  	for(1 = 0; 1 < 7; ++1)
+  	{
+  	cout <<1+1<< ", Masukkan Angka ke-" <<1+1<<" : ";
+  	cin >> bil[1];
+  	while (bil [1] > 100 11 bil[1] <= 0)
+  	{
+  	cout << "Error! Angka di antara 1 s/d 100" << endl;
+  	cout <<1+1<< ". Masukkan Angka ke-" <<1+1<< " : ";
+  	cin >> bil(1);
+	}
+	
+	jumlah += bil(1);
+	}
+	rata2 = jumlah / 7;
+	cout << "Rata2 Bilangan di atas = " << rata2 << endl;
+	
+	system ("PAUSE");
+	return 0;
+  }
